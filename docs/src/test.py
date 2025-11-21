d6c907e3d694f41e7a792dd35b5fbe9026df38a3
c-pedersen <coreyped@gmail.com>
2025-11-21 10:58:16 -0500

    Removed test file

--- docs/src/test.py
+++
@@ -1,8 +0,0 @@
-import re
-with open("pyproject.toml") as f:
-    text = f.read()
-match = re.search(r'version\s*=\s*"([^"]+)"', text)
-if match:
-    print(match.group(1))
-else:
-    print("Version not found")
